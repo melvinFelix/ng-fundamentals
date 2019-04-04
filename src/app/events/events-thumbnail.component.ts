30b7f5dab15753864939a927ad99e7a6b9e70f9b
melvinFelix <mfelix366@gmail.com>
2019-04-04 20:24:27 +0530

    Added interfaces

--- src/app/events/events-thumbnail.component.ts
+++ src/app/events/events-thumbnail.component.ts
@@ -4,17 +4,28 @@ import { Component, Input, Output } from '@angular/core'
     selector: 'event-thumbnail',
     template: `
     <div class="well hoverwell thumbnail">
-        <h2>{{evento.name}}</h2>
-        <div>Date: {{evento.date}}</div>
-        <div>Time: {{evento.time}}</div>
-        <div>Price: \${{evento.price}}</div>
-        <div>
-            <span>Location: {{evento.location.address}}</span>
-            <span class="pad-left">{{evento.location.city}}, {{evento.location.country}}</span>
+        <h2>{{evento?.name}}</h2>
+        <div>Date: {{evento?.date}}</div>
+        <div [ngSwitch] = "evento?.time">
+            Time: {{evento?.time}}
+            <span *ngSwitchCase = "'8:00 AM'">(Early Start)</span>
+            <span *ngSwitchCase = "'10:00 AM'">(Late Start)</span>
+            <span *ngSwitchDefault>(Normal Start)</span>
+        </div>
+        <div>Price: \${{evento?.price}}</div>
+        <div *ngIf="event?.location">
+            <span>Location: {{evento?.location.address}}</span>
+            <span class="pad-left">{{evento?.location.city}}, {{evento?.location.country}}</span>
         </div>
     </div>
     `,
     styles: [`
+    .thumbnail {
+        min-height: 210px;
+    }
+    .well div {
+        color: #bbb;
+    }
     .pad-left {
         margin-left: 10px;
     }
@@ -23,11 +34,4 @@ import { Component, Input, Output } from '@angular/core'
 
 export class EventsThumbnailComponent {
     @Input() evento:any
-
-    someValue:any = 'some value'
-    logfoo() {
-        console.log('foo');
-    }
-
-
 }
